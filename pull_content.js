cbf956bc24760b81523e6aca59ff87908ef4691a
launicorn <laura.capriles95@gmail.com>
2025-04-03 16:07:25 -0400

    Create pull_content.jsnotes

---
+++ pull_content.js
@@ -0,0 +1,12 @@
+//let url="https://docs.google.com/spreadsheets/d/e/2PACX-1vRDP]mwmnO18TLW1XUSZp1kwhrul4H
+//let data;
+//let originalAboutText = document.getElementById('about-text').innerHTML
+//fetch (url)
+ // .then(response => response.text())
+//.then(csvText => {
+ // const rows = cvsText.split('\n');
+ // data = rows map(row => row.split('\t')))
+
+//then(text => 
+  //.then(function(response){
+//tabseparate values on google sheets
