0204a9bd5638d8b92f1a3c3ca9f592380ea2bcfa
launicorn <laura.capriles95@gmail.com>
2025-04-17 13:10:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function goToSection(section){
   window.location.href = `${section}.html`;
 }
 
-// scrolls and stretch
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
   const image = document.getElementById('titleImage');
 
-  const scaleY = 1 + scrollY * 0.003; 
-  image.style.transform = `scaleY(${scaleY})`;
+  if (image) {
+    const scaleY = 1 + scrollY * 0.002;
+    image.style.transform = `scaleY(${scaleY})`;
+    image.style.transformOrigin = 'top center';
+  }
 });
