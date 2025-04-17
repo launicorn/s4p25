fc7c3005064b55e25111dd5f1719c9798c53048e
launicorn <laura.capriles95@gmail.com>
2025-04-17 15:43:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', () => {
   const image = document.getElementById('titleImage');
 
   if (image) {
-    const scaleY = 1 + scrollY * 0.0003;
+    const scaleY = 1 + scrollY * 0.03;
     image.style.transform = `scaleY(${scaleY})`;
     image.style.transformOrigin = 'top center';
   }
